e1598440f87ebe1fe52bf9c8198375b04d77fa94
Denn Orina <anyoksdenn@gmail.com>
2018-03-18 10:43:35 +0000

    set pty to true

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,8 @@ set :application, "travelWatamu"
 set :repo_url, "git@bitbucket.org:dennOrina/travelwatamu.git"
 
 set :passenger_restart_with_touch, true
+
+set :pty, true
 ssh_options[:auth_methods] = ["publickey"]
 ssh_options[:keys] = ["/home/vagrant/.ssh/id_rsa"]
 
