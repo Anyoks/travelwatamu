2696f1a40380e347d3c6a2d7eaea98212993a43b
Denn Orina <anyoksdenn@gmail.com>
2018-07-20 08:13:30 +0000

    added images to public directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/deploy/travelWatamu"
 # 
 set :bundle_binstubs, nil
 set :linked_files, %w{config/database.yml config/secrets.yml config/initializers/devise.rb}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets public/images}
 
 # cap production deploy:upload_yml
 namespace :deploy do
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -23,11 +23,6 @@ server '138.68.84.169', user: 'deploy', roles: %w{web app db}
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-
-TravelWatamu::Application.configure do
-config.serve_static_files = true
-end
-
 # Configuration
 # =============
 # You can set any configuration variable like in config/deploy.rb
